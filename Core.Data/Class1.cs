0e340426381f584da20c4a11155e4f5d8bc2885d
Alexis <alelesteyme@gmail.com>
2024-08-24 16:06:55 -0300

    Add. Primera Parte API Producto

--- Core.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Core.Data
-{
-    public class Class1
-    {
-
-    }
-}
--- Master_API/Controllers/ProductoController.cs
+++ Master_API/Controllers/ProductoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Master_API.Controllers
 {
@@ -7,5 +8,17 @@ namespace Master_API.Controllers
     [ApiController]
     public class ProductoController : ControllerBase
     {
+        [HttpGet("{ProductoID}")]
+        public async Task<ActionResult<Productos>> GetProduct(int ProductoID)
+        {
+            var product = await _context.Products.FindAsync(ProductoID);
+
+            if (product == null)
+            {
+                return GetProduct();
+            }
+
+            return product;
+        }
     }
 }
