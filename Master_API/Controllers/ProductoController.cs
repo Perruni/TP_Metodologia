81ab4282b8a6655fac3cbc98d4e986dcf22403af
Marcos Malveira <163179361+Perruni@users.noreply.github.com>
2024-08-24 15:16:26 -0300

    ADD: Se añadieron controladores vacios a la API

---
+++ Master_API/Controllers/ProductoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Master_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+    }
+}
