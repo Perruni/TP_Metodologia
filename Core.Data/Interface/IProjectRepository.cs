60e909bafd54625cf3586e8e2f233aeeec203f67
Marcos Malveira <163179361+Perruni@users.noreply.github.com>
2024-09-04 18:04:19 -0300

    Add: Subastas al proyectrepository

--- Core.Data/Interface/IProjectRepository.cs
+++ Core.Data/Interface/IProjectRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace Core.Data.Interface
 {
-    internal class IProjectRepository
+    public interface IProjectRepository
     {
+        public void AddSubasta(Subasta subasta);
+        public void DeleteSubasta(int subastaId);
+        public List<Subasta> GetAllSubasta();
+        public Subasta GetSubastabyID(int id);
+        public void UpdateSubasta(Subasta subasta, int subastaId);
+        public void HabilitarSubasta(int subastaID, int estadoSubasta);
+
     }
 }
